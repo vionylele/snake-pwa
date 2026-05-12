b363bf18c2026186ce9defb8270c6d389e940ffb
vionylele <vionylee07@gmail.com>
2026-05-12 16:15:40 +0800

    Initial commit: Snake PWA with retro neon arcade aesthetic

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'snake-pwa-v1';
+const ASSETS = [
+  '/snake-pwa/',
+  '/snake-pwa/index.html',
+  '/snake-pwa/manifest.json',
+  '/snake-pwa/icon-192.png',
+  '/snake-pwa/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then((response) => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+  );
+});
